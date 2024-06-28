0960f923a51bc8f713d30891ed8bc577dc3d909b
Youness Bourgui <younessbourgui07@gmail.com>
2024-06-28 23:19:50 +0200

    Create The Service Layer
    resolve #5

---
+++ src/main/java/main/service/CarService.java
@@ -0,0 +1,98 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.service;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+import main.dto.CarDTO;
+import main.entity.Car;
+import main.mapper.CarMapper;
+import main.repo.CarRepo;
+import main.repo.OwnerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author hp
+ */
+@Service
+public class CarService {
+    @Autowired
+    public CarService(CarRepo repo, OwnerRepo repo1, CarMapper mapper) {
+        this.repo = repo;
+        this.mapper = mapper;
+        this.repo1=repo1;
+    }
+    @PersistenceContext
+    private EntityManager em;
+    
+    
+    private CarRepo repo;
+    private CarMapper mapper;
+    private OwnerRepo repo1;
+    
+    public CarDTO findCar(Integer id){
+        var c = repo.findById(id).orElse(null);
+        return mapper.toDTO(c);
+    }
+    
+    @Transactional
+    public CarDTO createCar(CarDTO x){
+        var car = mapper.toCar(x);
+        var savedCar = repo.save(car);
+        return mapper.toDTO(savedCar);
+    }
+    
+    @Transactional
+    public CarDTO updateCar(Integer id, CarDTO x){
+        var car = repo.findById(id).orElse(null);
+        car.setBrand(x.getBrand());
+        car.setColor(x.getColor());
+        car.setModelYear(x.getModelYear());
+        car.setPrice(x.getPrice());
+        car.setRegistrationNumber(x.getRegistrationNumber());
+        car.setOwner(repo1.findById(x.getOwnerID()).orElse(null));
+        var savedCar = repo.save(car);
+        return mapper.toDTO(savedCar);
+    }
+    
+    public List<CarDTO> getAll(){
+        return repo.findAll().stream().map(x -> mapper.toDTO(x)).collect(Collectors.toList());
+    }
+    
+    @Transactional
+    public void deleteCar(Integer id){
+        repo.deleteById(id);
+    }
+    
+    public List<Car> getCarsByBrand(String brand){
+        var q = "SELECT * FROM car WHERE brand= :brand";
+        return em.createNativeQuery(q, Car.class).setParameter("brand", brand).getResultList();
+    }
+    
+    public List<Car> getCarsByYear(int year){
+        var q = "SELECT * FROM car WHERE model_year= :x";
+        return em.createNativeQuery(q, Car.class).setParameter("x", year).getResultList();
+    }
+    
+    public List<Car> getCarsByColor(String color){
+        var q = "SELECT * FROM car WHERE color= :x";
+        return em.createNativeQuery(q, Car.class).setParameter("x", color).getResultList();
+    }
+    
+    public List<Car> getCarsByColorOrBrand(String color, String brand){
+        var q = "SELECT * FROM car WHERE color= :color OR brand= :brand";
+        return em.createNativeQuery(q, Car.class).setParameter("brand", brand).setParameter("color",color).getResultList();
+    }
+    
+    public List<Car> getCarsByBrandSortByYear(String brand){
+        var q = "SELECT * FROM car WHERE brand= :brand ORDER BY model_year";
+        return em.createNativeQuery(q, Car.class).setParameter("brand", brand).getResultList();
+    }
+}
---
+++ src/main/java/main/service/OwnerService.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.service;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.NonUniqueResultException;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+import main.dto.OwnerDTO;
+import main.entity.Owner;
+import main.mapper.OwnerMapper;
+import main.repo.CarRepo;
+import main.repo.OwnerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author hp
+ */
+@Service
+public class OwnerService {
+    @Autowired
+    public OwnerService(OwnerMapper mapper, OwnerRepo repo, CarRepo carRepo) {
+        this.mapper = mapper;
+        this.repo = repo;
+        this.carRepo = carRepo;
+    }
+    @PersistenceContext
+    private EntityManager em;
+    private OwnerMapper mapper;
+    private OwnerRepo repo;
+    private CarRepo carRepo;
+    
+    public List<OwnerDTO> getAll(){
+        return repo.findAll().stream().map(o -> mapper.toDTO(o)).collect(Collectors.toList());
+    }
+    
+    public OwnerDTO findOwner(Integer id){
+        var o = repo.findById(id).orElse(null);
+        return mapper.toDTO(o);
+    }
+    
+    @Transactional
+    public void deleteOwner(Integer id){
+        repo.deleteById(id);
+    }
+    
+    @Transactional
+    public OwnerDTO createOwner(OwnerDTO x){
+        var o = mapper.toOwner(x);
+        var savedOwner = repo.save(o);
+        return mapper.toDTO(savedOwner);
+    }
+    
+    @Transactional
+    public OwnerDTO updateOwner(Integer id, OwnerDTO x){
+        var o = repo.findById(id).orElse(null);
+        o.setFirstName(x.getFirstName());
+        o.setLastName(x.getLastName());
+        o.setCars(carRepo.findAllById(x.getCarsIDs()));
+        var saved = repo.save(o);
+        return mapper.toDTO(saved);
+    }
+    
+    public Owner findOwnerByFirstName(String firstName){
+        var q = "SELECT * FROM owner WHERE firstname= :x";
+        try{
+            return(Owner)  em.createNativeQuery(q, Owner.class).setParameter("x", firstName).getSingleResult();
+        }catch(NoResultException e){
+            return null;
+        }catch(NonUniqueResultException e){
+            return null;
+        }
+    }
+}
