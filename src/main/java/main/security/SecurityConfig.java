2283f91311ee26c9ddd150c127b4c8be21d81ffe
Youness Bourgui <younessbourgui07@gmail.com>
2024-06-30 23:51:40 +0200

    Secure The API Using Basic Auth

---
+++ src/main/java/main/security/SecurityConfig.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ *
+ * @author hp
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer -> configurer
+                .requestMatchers(HttpMethod.GET, "/api/owners").hasAnyRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/api/owenrs/**").hasAnyRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/api/owners").hasAnyRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/api/owners/**").hasAnyRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/api/owners/**").hasAnyRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/api/cars").hasAnyRole("GUEST", "ADMIN")
+                .requestMatchers(HttpMethod.GET, "/api/cars/**").hasAnyRole("GUEST", "ADMIN")
+                .requestMatchers(HttpMethod.POST, "/api/cars").hasAnyRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/api/cars/**").hasAnyRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/api/cars/**").hasAnyRole("ADMIN")
+        );
+        
+        http.httpBasic(Customizer.withDefaults());
+        http.csrf(x -> x.disable());
+        return http.build();
+    }
+    
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        var youness = User.builder()
+                .username("youness")
+                .password("{bcrypt}$2a$10$LP/B2sSTqBhc9pcceCcK5epRvXxGMFqklyhCVYtzszUwXBT11NYpC")
+                .roles("ADMIN")
+                .build();
+        
+        var yassine = User.builder()
+                .username("yassine")
+                .password("{bcrypt}$2a$10$IJGXe6xnJ/f5yBxjsnSg1.ARl0hOOrRJaQpoK4DMchxcd4.2Jz2oG")
+                .roles("GUEST")
+                .build();
+        
+        return new InMemoryUserDetailsManager(youness, yassine);
+    }
+}
